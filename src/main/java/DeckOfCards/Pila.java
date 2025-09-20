fc747c47392e8da666bee0e2b074298d2fbc69de
PC OSTRICH <a1197503@uabc.edu.mx>
2025-09-19 20:01:30 -0700

    Version final del solitario con la implementacion de la pila. 1.0

--- src/main/java/DeckOfCards/Pila.java
+++ src/main/java/DeckOfCards/Pila.java
@@ -1,18 +1,22 @@
 package DeckOfCards;
 
+// Se importan las librerias necesarias para el funcionamiento de la clase.
 import java.util.Iterator;
 
+// Se declara la creacion de la clase Pila la cual esta casteada para un elemento y implementa el iterable
 public class Pila<T> implements Iterable<T> {
-    private T[] pila;
-    private int tope;
-    private int size;
+    private T[] pila; // Arreglo base que almacena los datos de la pila
+    private int tope; // Indice del elemento que esta en la cima
+    private int size; // Tamaño maximo de la pila
 
+    // Metodo constructor el cual crea una pila vacia de cierto tamaño
     public Pila(int size) {
         pila = (T[]) new Object[size];
         tope = -1;
         this.size = size;
     }
 
+    // Metodo para insertar un dato en el tope de la pila
     public void push(T dato) {
         if (estaLlena()) {
             throw new IllegalStateException("La pila está llena");
@@ -21,12 +25,7 @@ public class Pila<T> implements Iterable<T> {
         pila[tope] = dato;
     }
 
-    public void pushTodo(Pila<T> otraPila) {
-        for (int i = 0; i <= otraPila.tope; i++) {
-            this.push(otraPila.pila[i]);
-        }
-    }
-
+    // Metodo que se encarga de eliminar y devolver el elemento que se encuentre en el tope
     public T pop() {
         if(estaVacia()) {
             throw new IllegalStateException("La pila está vacía");
@@ -37,6 +36,7 @@ public class Pila<T> implements Iterable<T> {
         return dato;
     }
 
+    // Metodo que devuelve sin eliminar el elemento que se encuentra en el tope
     public T peek() {
         if(estaVacia()) {
             throw new IllegalStateException("La pila está vacía");
@@ -44,6 +44,7 @@ public class Pila<T> implements Iterable<T> {
         return pila[tope];
     }
 
+    // Metodo que permite un iterador sobre la pila
     public Iterator<T> iterator() {
         return new Iterator<T>() {
             private int indice = tope;
@@ -62,12 +63,15 @@ public class Pila<T> implements Iterable<T> {
         };
     }
 
+    // Indica si la pila no tiene elementos
     public boolean estaVacia() {
         return tope == -1;
     }
+    // Indica si la pila alcanzó su capacidad máxima
     public boolean estaLlena() {
         return tope == size - 1;
     }
+    // Devuelve la cantidad actual de elementos en la pila
     public int tamañoDePila() {
         return tope + 1;
     }
